eadb1a50463998cc472c604da971d733baf9a3be
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-03 19:39:16 +0600

    database added

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,12 @@ const port = 5000
 
 async function server() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect(`${process.env.DATABASE_URL}`);
+    console.log('connected to database')
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
   })
   } catch (error) {
     console.log(error)
